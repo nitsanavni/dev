2c8483f6624fe8467413328debfeea4aa4121aab
Nitsan Avni <nitsanav@gmail.com>
2023-07-10 15:10:06 +0000

    black

--- chat.py
+++ chat.py
@@ -4,38 +4,38 @@ import json
 import os
 
 
-def chat_with_gpt4(api_key, text):
+def chat_with_gpt4(text):
     api_key = os.getenv("OPENAI_API_KEY")
     if not api_key:
-        raise Exception(
-            "Couldn't find the 'OPENAI_API_KEY' environment variable!")
+        raise Exception("Couldn't find the 'OPENAI_API_KEY' environment variable!")
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {api_key}',
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
     }
     data = {
         "model": "gpt-4",
-        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": text}]
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text},
+        ],
     }
 
     response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
-        return response.json()['choices'][0]['message']['content']
+        return response.json()["choices"][0]["message"]["content"]
     else:
         raise Exception(
-            f'Request to GPT-4 API failed with status {response.status_code}. The response is: {response.text}')
+            f"Request to GPT-4 API failed with status {response.status_code}. The response is: {response.text}"
+        )
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "text", help="The text to be used as prompt for GPT-4.")
+    parser.add_argument("text", help="The text to be used as prompt for GPT-4.")
     args = parser.parse_args()
 
-    # Please replace 'your-api-key' with your actual OpenAI GPT-4 API Key
-    api_key = 'your-api-key'
-    print(chat_with_gpt4(api_key, args.text))
+    print(chat_with_gpt4(args.text))
 
 
 if __name__ == "__main__":
